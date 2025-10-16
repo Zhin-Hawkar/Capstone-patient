92e402b2116de769c0d73d7cefea0e33c05dc5c0
zhin hawkar <zhinhawkar@gmail.com>
2025-10-16 11:35:32 +0300

    server host added to host path

--- lib/Constants/enum.dart
+++ lib/Constants/enum.dart
@@ -1,5 +1,5 @@
 class EnumValues {
-  static final String HOST = "http://10.0.2.2:8000/";
+  static final String HOST = "https://capstone-backend-inbqo.sevalla.app/";
   static final String WIFI_IPV4 = "10.1.1.141";
   static final String DEVICE_FIRST_OPEN = "10.0.2.2";
   static final String IDE_EMULATOR_IP = "DEVICE_FIRST_OPEN";
