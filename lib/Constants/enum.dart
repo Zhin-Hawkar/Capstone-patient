d77b48d9679a4e3e1875885eb4364c92294bda23
zhin hawkar <zhinhawkar@gmail.com>
2025-10-21 12:03:46 +0300

    enum modefied

--- lib/Constants/enum.dart
+++ lib/Constants/enum.dart
@@ -1,6 +1,6 @@
 class EnumValues {
-  // static final String HOST = "https://capstone-backend-inbqo.sevalla.app/";
-  static final String HOST = "http://10.1.1.141:8000/";
+  // static final String HOST = "http://10.1.1.141:8000/";
+  static final String HOST = "https://capstone-backend-inbqo.sevalla.app/";
   static final String WIFI_IPV4 = "10.1.1.141";
   static final String IDE_EMULATOR_IP = "10.0.2.2";
   static final String DEVICE_FIRST_OPEN = "DEVICE_FIRST_OPEN";
